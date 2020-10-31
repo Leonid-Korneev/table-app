b1b74eefd1e47744a8aff136fcc8a44d570c9b55
Leonid <lkorneev98@rambler.ru>
2020-10-31 16:11:21 +0800

    Updates

---
+++ precache-manifest.bfab4ad4e033b45a6b40adc526181678.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6853b7e16b48a4612e01b3e983d7b267",
+    "url": "/table-app/index.html"
+  },
+  {
+    "revision": "47ec248e567b5cf73942",
+    "url": "/table-app/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "3ecddbfb09a36350941d",
+    "url": "/table-app/static/css/main.224d6921.chunk.css"
+  },
+  {
+    "revision": "47ec248e567b5cf73942",
+    "url": "/table-app/static/js/2.e378ce22.chunk.js"
+  },
+  {
+    "revision": "63578bd18f7d62dbc9a1cb42d77740d7",
+    "url": "/table-app/static/js/2.e378ce22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ecddbfb09a36350941d",
+    "url": "/table-app/static/js/main.81e26e42.chunk.js"
+  },
+  {
+    "revision": "c341ff895c068c6de660",
+    "url": "/table-app/static/js/runtime-main.4b0aad7f.js"
+  }
+]);
